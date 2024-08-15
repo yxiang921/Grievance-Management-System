86af0d9d2c4d7c7fc17e60b7242dc655c1c53287
yxiang921 <xiangf35@gmail.com>
2024-08-15 11:46:13 +0800

    Modified: layouts/app.blade, customize color set.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,31 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                green: {
+                    100: "#BAEADA",
+                    900: "#00B074",
+                },
+                purple: "#F3F2F7",
+                yellow: {
+                    100: "#F2F1D3",
+                    900: "#B0AB29",
+                },
+                orange: {
+                    100: "#F5DEBD",
+                    900: "#DC8E1A",
+                },
+            },
+        },
+    },
+    plugins: [],
+};
