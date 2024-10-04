1b647352088bdee1757ff3494e778bfcae5a68c6
yxiang921 <xiangf35@gmail.com>
2024-10-04 17:19:58 +0800

    Modify: AdminController

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -10,12 +10,6 @@ use DB;
 
 class AdminController extends Controller
 {
-    // To Do
-    // C: Create admin 201
-    // R: Read admin 200
-    // U: Update admin 200
-    // D: Delete admin 200
-
     public function __construct()
     {
 
