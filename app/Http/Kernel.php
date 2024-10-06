81567f22d6c0bddac57a8df2428cda6bbd362bff
yxiang921 <xiangf35@gmail.com>
2024-10-06 22:04:48 +0800

    Feat: Auth middleware for both admin and department

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        'admin.auth' => \App\Http\Middleware\AdminAuth::class,
+        'department.auth' => \App\Http\Middleware\DepartmentAuth::class,
     ];
 }
--- routes/admin.php
+++ routes/admin.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Route;
 Route::group([
     'prefix' => 'admin',
     'as' => 'admin.',
+    'middleware' => ['admin.auth']
     
 ], function () {
 
