98bf2c246802f29a125bb6dd6c7accb9225164d6
yxiang921 <xiangf35@gmail.com>
2024-10-10 17:02:25 +0800

    Feat: Admin setting page

---
+++ app/Http/Controllers/Admin/SettingController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use Auth;
+use Illuminate\Http\Request;
+
+class SettingController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+
+        $admin_id = Auth::guard('admin')->user()->id;
+        $admin = Admin::find($admin_id);
+
+        return view('admin.settings', [
+            'admin' => $admin,
+        ]);
+    }
+
+    public function update()
+    {
+        $req = request();
+
+        $admin_id = Auth::guard('admin')->user()->id;
+
+        $admin = Admin::find($admin_id);
+
+
+        $admin->admin_name = $req->name;
+        $admin->admin_username = $req->admin_username;
+        $admin->admin_phone = $req->admin_phone;
+        $admin->admin_email = $req->admin_email;
+
+        if ($req->admin_password != '') {
+            $admin->admin_password = bcrypt($req->admin_password);
+        }
+
+        $admin->save();
+
+        return redirect()->route('admin.settings');
+    }
+}
--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
     <div class="container mx-auto">
-        <div class="mx-auto bg-white border border-gray-100 p-4 rounded-md">
+        <form action="{{ route('admin.setting.update') }}" method="POST"
+            class="mx-auto bg-white border border-gray-100 p-4 rounded-md">
+            @csrf
             <h2 class="text-2xl font-bold mb-6 text-gray-800">Edit Your Profile</h2>
 
             <table class="w-2/3">
@@ -10,62 +12,51 @@
                     <tr>
                         <td class="font-bold text-slate-700 py-2">Admin ID</td>
                         <td class="py-2">
-                            <input type="text" name="admin_id" value="ADM01"
-                                class="w-full primary-input"
+                            <input type="text" name="admin_id" value="ADM{{ $admin->id }}" class="w-full primary-input"
                                 readonly>
                         </td>
                     </tr>
                     <tr>
-                        <td class="font-bold text-slate-700 py-2">First Name</td>
+                        <td class="font-bold text-slate-700 py-2">Full Name</td>
                         <td class="py-2">
-                            <input type="text" name="first_name" value="John"
-                                class="w-full primary-input">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="font-bold text-slate-700 py-2">Last Name</td>
-                        <td class="py-2">
-                            <input type="text" name="last_name" value="Doe"
+                            <input type="text" name="last_name" value="{{ $admin->admin_name }}"
                                 class="w-full primary-input">
                         </td>
                     </tr>
                     <tr>
                         <td class="font-bold text-slate-700 py-2">Username</td>
                         <td class="py-2">
-                            <input type="text" name="username" value="Doe"
+                            <input type="text" name="username" value="{{ $admin->admin_username }}"
                                 class="w-full primary-input">
                         </td>
                     </tr>
                     <tr>
                         <td class="font-bold text-slate-700 py-2">Email</td>
                         <td class="py-2">
-                            <input type="email" name="email" value="johndoe@gmail.com"
+                            <input type="email" name="email" value="{{ $admin->admin_email }}"
                                 class="w-full primary-input">
                         </td>
                     </tr>
                     <tr>
                         <td class="font-bold text-slate-700 py-2">Phone Number</td>
                         <td class="py-2">
-                            <input type="text" name="phone_number" value="012 3456789"
+                            <input type="text" name="phone_number" value="{{ $admin->admin_phone_number }}"
                                 class="w-full primary-input">
                         </td>
                     </tr>
                     <tr>
-                        <td class="font-bold text-slate-700 py-2">Password</td>
+                        <td class="font-bold text-slate-700 py-2">Reset Password</td>
                         <td class="py-2">
-                            <input type="password" name="password" value="********"
-                                class="w-full primary-input">
+                            <input type="password" name="password" value="" class="w-full primary-input">
                         </td>
                     </tr>
                 </tbody>
             </table>
 
             <div class="mt-6 flex justify-end space-x-4">
-                <button
-                    class="cancel-btn">Cancel</button>
-                <button
-                    class="primary-btn">Submit</button>
+                <button class="cancel-btn">Cancel</button>
+                <button class="primary-btn">Submit</button>
             </div>
-        </div>
+        </form>
     </div>
 @endsection
