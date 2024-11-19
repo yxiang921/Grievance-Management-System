7c1aa0298baa641a16ce30ecddbedf5a2b3969d9
yxiang921 <xiangf35@gmail.com>
2024-11-19 15:42:59 +0800

    Feat: installed laravel passport

--- app/Http/Controllers/Admin/GrievanceController.php
+++ app/Http/Controllers/Admin/GrievanceController.php
@@ -69,6 +69,7 @@ class GrievanceController extends Controller
         $grievance = Grievance::find($grievance_id);
 
         $grievance->department_id = $validateData['departmentID'];
+        $grievance->status = 'In Progress';
         $grievance->priority = $validateData['priority'];
         $grievance->due_date = $validateData['duedate'];
         $grievance->category = $validateData['category'];
