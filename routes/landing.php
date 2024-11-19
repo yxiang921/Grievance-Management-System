d20ac5c0049d5ac46fd19ebf490974b0f3077111
AaaaaaaaaronL <lwichunkiat6666@gmail.com>
2024-11-19 15:48:53 +0800

    settle index interface

--- routes/landing.php
+++ routes/landing.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,14 +15,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//homepage
 Route::get('/', function () {
-    return view('home');
+    return view('landing.index'); // or landing.index if renamed
 });
 
+// Landing Page Route
+Route::get('/landing', [LandingController::class, 'index']);
+
+
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 
 // require __DIR__ . '/auth.php';
 require __DIR__ . '/admin.php';
 require __DIR__ . '/department.php';
-require __DIR__ . '/landing.php'; 
 require __DIR__ . '/custom_auth.php';
