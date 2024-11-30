4a53ca9c645758534de0216ecb3c45c0c729140f
yxiang921 <xiangf35@gmail.com>
2024-11-30 18:26:19 +0800

    Modify: landing page

--- routes/landing.php
+++ routes/landing.php
@@ -15,30 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-//homepage
 Route::get('/', function () {
-    return view('landing.index'); // or landing.index if renamed
+    return view('landing.index');
 });
-
-
 
 Route::get('/faq', [LandingController::class, 'faq'])
 ->name('faq');
 
 Route::get('/tac', [LandingController::class, 'tac'])
 ->name('tac');
-
-
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-
-// require __DIR__ . '/auth.php';
-require __DIR__ . '/admin.php';
-require __DIR__ . '/department.php';
-require __DIR__ . '/custom_auth.php';
